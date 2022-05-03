be4fcb7ec9cbb2b1a4c4b606cfb60bf0d88e3ffe
david.giczi@gmail.com <david.giczi@gmail.com>
2022-05-03 18:37:24 +0200

    Developing displayer

--- src/SneakerBoard.js
+++ src/SneakerBoard.js
@@ -4,13 +4,30 @@ function SneakerBoard(props){
     return(<div className='Sneaker-Board'>
         <img src={props.image} alt='Nice-tree'></img>
         <SnakerBoardFields/>
+        <SnakeNavigationButtons className='Navigation-Button'/>
     </div>)
 }
 
 function SnakerBoardFields(){
     return(<>
-    {SNAKER_BOARD_FIELDS.map((field) => <div key={ field.id } style={{backgroundColor: field.color}} className='Board-Field'>{field.id}</div>)}
+    {SNAKER_BOARD_FIELDS.map((field) => 
+    <div key={ field.id } style={{backgroundColor: field.color}} className='Board-Field'>{field.id}</div>)}
     </>);
+}
+
+function SnakeNavigationButtons(){
+return(<div className='Navigation-btn'>
+<div className='Up-btn'>
+<button id='up-btn'>Fel</button>
+</div>
+<div>
+<button id='left-btn'>Balra</button>
+<button id='right-btn'>Jobbra</button>
+</div>
+<div>
+<button id='down-btn'>Le</button>
+</div>
+</div>);
 }
 
 export default SneakerBoard;
