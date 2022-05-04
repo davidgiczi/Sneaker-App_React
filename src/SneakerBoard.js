172d986378a87e55bbfb1bf73967b438003295f6
david.giczi@gmail.com <david.giczi@gmail.com>
2022-05-04 17:09:44 +0200

    Displayer

--- src/SneakerBoard.js
+++ src/SneakerBoard.js
@@ -1,31 +1,45 @@
-import { SNAKER_BOARD_FIELDS } from "./SneakerLogic";
+import { SNAKER_BOARD_FIELDS } from './SneakerLogic';
+import upArrow from './icons/up.jpg';
+import downArrow from './icons/down.jpg';
+import leftArrow from './icons/left.jpg';
+import rightArrow from './icons/right.jpg';
 
 function SneakerBoard(props){
     return(<div className='Sneaker-Board'>
         <img src={props.image} alt='Nice-tree'></img>
         <SnakerBoardFields/>
-        <SnakeNavigationButtons className='Navigation-Button'/>
+        <SnakeNavigationButtons className='Navigation-Button' 
+        up = {'url(' +  upArrow + ')' }
+        down = {'url(' +  downArrow + ')' }
+        left = {'url(' +  leftArrow + ')' }
+        right = {'url(' +  rightArrow + ')' }
+        />
     </div>)
 }
 
 function SnakerBoardFields(){
+
     return(<>
     {SNAKER_BOARD_FIELDS.map((field) => 
-    <div key={ field.id } style={{backgroundColor: field.color}} className='Board-Field'>{field.id}</div>)}
+    <div key={ field.id } style={{backgroundColor: field.color, 
+                                    borderTop: field.borderTop,
+                                    borderRight: field.borderRight,
+                                    borderBottom: field.borderBottom,
+                                    borderLeft: field.borderLeft}} className='Board-Field'>{field.id}</div>)}
     </>);
 }
 
-function SnakeNavigationButtons(){
+function SnakeNavigationButtons(props){
 return(<div className='Navigation-btn'>
 <div className='Up-btn'>
-<button id='up-btn'>Fel</button>
+<button id='up-btn' style={{backgroundImage : props.up}}></button>
 </div>
 <div>
-<button id='left-btn'>Balra</button>
-<button id='right-btn'>Jobbra</button>
+<button id='left-btn' style={{backgroundImage : props.left}}></button>
+<button id='right-btn' style={{backgroundImage : props.right}}></button>
 </div>
 <div>
-<button id='down-btn'>Le</button>
+<button id='down-btn' style={{backgroundImage : props.down}}></button>
 </div>
 </div>);
 }
--- src/SneakerLogic.js
+++ src/SneakerLogic.js
@@ -1,6 +1,12 @@
+
 const TABLE_SIZE = 15 * 15;
 let boardField = {id: null,
-              color: null}
+              color: null,
+            borderTop: null,
+            borderRight: null,
+            borderBottom: null,
+            borderLeft: null
+        }
 let SNAKER_BOARD_FIELDS = initGame();
 
 
@@ -10,7 +16,19 @@ function initGame(){
 
         boardField = {};
         boardField.id = i;
-        boardField.color = 'white';
+        boardField.color = 'transparent';
+        if( i < 15 ){
+            boardField.borderTop = '5px solid #818c3c';
+        }
+        if( i % 15 === 14 ){
+            boardField.borderRight = '5px solid #818c3c';
+        }
+        if( i > 209 && i < 225 ){
+            boardField.borderBottom = '5px solid #818c3c';
+        }
+        if( i % 15 === 0 ){
+            boardField.borderLeft = '5px solid #818c3c';
+        }
         fieldStore.push(boardField);
     }
     return fieldStore;
